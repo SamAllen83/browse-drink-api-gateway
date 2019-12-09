7cc050796280a5da00a5d480e0a3737215c57a11
Sam Allen <Sam.Allen@beveragebot.com>
2019-12-08 23:33:37 -0800

    first commit

---
+++ src/main/java/com/bevbot/browse/drink/api/gateway/BrowseDrinkApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.bevbot.browse.drink.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrowseDrinkApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrowseDrinkApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bevbot/browse/drink/api/gateway/BrowseDrinkApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bevbot.browse.drink.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrowseDrinkApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
