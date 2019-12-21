84c3cea940b5ec9766ffaaa480df130fc5900aea
Sam Allen <Sam.Allen@beveragebot.com>
2019-12-21 01:37:50 -0800

    Adding microfront ends

--- src/main/java/com/bevbot/browse/drink/api/gateway/BrowseDrinkApiGatewayApplication.java
+++ src/main/java/com/bevbot/browse/drink/api/gateway/BrowseDrinkApiGatewayApplication.java
@@ -2,12 +2,23 @@ package com.bevbot.browse.drink.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @SpringBootApplication
 public class BrowseDrinkApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BrowseDrinkApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public SecurityWebFilterChain staticContentSpringSecurityFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange()
+                .anyExchange()
+                .permitAll();
+        return http.build();
     }
 
 }
