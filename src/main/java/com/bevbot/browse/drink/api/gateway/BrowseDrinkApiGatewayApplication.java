e24d9f7da8a721f94575e900031df505d0e73ec1
Sam Allen <sallen7@wsgc.com>
2019-12-27 13:44:40 -0800

    Connected browse-drink-api-gateway and bartender-api

--- src/main/java/com/bevbot/browse/drink/api/gateway/BrowseDrinkApiGatewayApplication.java
+++ src/main/java/com/bevbot/browse/drink/api/gateway/BrowseDrinkApiGatewayApplication.java
@@ -2,6 +2,9 @@ package com.bevbot.browse.drink.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BrowseDrinkApiGatewayApplication {
@@ -9,5 +12,28 @@ public class BrowseDrinkApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(BrowseDrinkApiGatewayApplication.class, args);
     }
+
+//    @Bean
+//    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route("path_route", r -> r.path("/get")
+//                        .uri("http://httpbin.org"))
+//                .route("host_route", r -> r.host("*.myhost.org")
+//                        .uri("http://httpbin.org"))
+//                .route("rewrite_route", r -> r.host("*.rewrite.org")
+//                        .filters(f -> f.rewritePath("/foo/(?<segment>.*)", "/${segment}"))
+//                        .uri("http://httpbin.org"))
+//                .route("hystrix_route", r -> r.host("*.hystrix.org")
+//                        .filters(f -> f.hystrix(c -> c.setName("slowcmd")))
+//                        .uri("http://httpbin.org"))
+//                .route("hystrix_fallback_route", r -> r.host("*.hystrixfallback.org")
+//                        .filters(f -> f.hystrix(c -> c.setName("slowcmd").setFallbackUri("forward:/hystrixfallback")))
+//                        .uri("http://httpbin.org"))
+//                .route("limit_route", r -> r
+//                        .host("*.limited.org").and().path("/anything/**")
+//                        .filters(f -> f.requestRateLimiter(c -> c.setRateLimiter(redisRateLimiter())))
+//                        .uri("http://httpbin.org"))
+//                .build();
+//    }
 
 }
