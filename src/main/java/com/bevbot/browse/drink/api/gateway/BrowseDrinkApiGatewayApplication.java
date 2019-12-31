ec285e7168279e7d6222f99b7ca58497c5dd3008
Sam Allen <sallen7@wsgc.com>
2019-12-30 23:57:04 -0800

    Everything is working!

--- src/main/java/com/bevbot/browse/drink/api/gateway/BrowseDrinkApiGatewayApplication.java
+++ src/main/java/com/bevbot/browse/drink/api/gateway/BrowseDrinkApiGatewayApplication.java
@@ -2,9 +2,8 @@ package com.bevbot.browse.drink.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class BrowseDrinkApiGatewayApplication {
@@ -13,27 +12,9 @@ public class BrowseDrinkApiGatewayApplication {
         SpringApplication.run(BrowseDrinkApiGatewayApplication.class, args);
     }
 
-//    @Bean
-//    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-//        return builder.routes()
-//                .route("path_route", r -> r.path("/get")
-//                        .uri("http://httpbin.org"))
-//                .route("host_route", r -> r.host("*.myhost.org")
-//                        .uri("http://httpbin.org"))
-//                .route("rewrite_route", r -> r.host("*.rewrite.org")
-//                        .filters(f -> f.rewritePath("/foo/(?<segment>.*)", "/${segment}"))
-//                        .uri("http://httpbin.org"))
-//                .route("hystrix_route", r -> r.host("*.hystrix.org")
-//                        .filters(f -> f.hystrix(c -> c.setName("slowcmd")))
-//                        .uri("http://httpbin.org"))
-//                .route("hystrix_fallback_route", r -> r.host("*.hystrixfallback.org")
-//                        .filters(f -> f.hystrix(c -> c.setName("slowcmd").setFallbackUri("forward:/hystrixfallback")))
-//                        .uri("http://httpbin.org"))
-//                .route("limit_route", r -> r
-//                        .host("*.limited.org").and().path("/anything/**")
-//                        .filters(f -> f.requestRateLimiter(c -> c.setRateLimiter(redisRateLimiter())))
-//                        .uri("http://httpbin.org"))
-//                .build();
-//    }
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
 }
--- src/main/java/com/bevbot/browse/drink/api/gateway/browse/BrowseProxy.java
+++ src/main/java/com/bevbot/browse/drink/api/gateway/browse/BrowseProxy.java
@@ -1,17 +1,23 @@
 package com.bevbot.browse.drink.api.gateway.browse;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UncheckedIOException;
 import java.net.URI;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.gateway.webflux.ProxyExchange;
-import org.springframework.http.MediaType;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.BodyInserters;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
+import org.springframework.web.client.RestTemplate;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 @RestController
 public class BrowseProxy {
@@ -20,26 +26,31 @@ public class BrowseProxy {
     @Value("${bartender.api.graph.ql.url}")
     private URI bartenderApiGraphQlUrl;
 
-    @GetMapping("/browse")
-    public Mono<String> browse(ProxyExchange<byte[]> proxy) {
-        String orderDrinkMutation ="mutation {\n" + "    orderDrink(customerName: \"Sam\", drinkName: \"Beer\") {\n" + "        customerName\n" + "        drinkName\n"
-                + "        orderId\n" + "    }\n" + "}";
+    @Autowired
+    private ResourceLoader resourceLoader;
 
-        GraphQLQuery topologyQuery = new GraphQLQuery();
-        topologyQuery.setQuery(orderDrinkMutation);
+    @Autowired
+    private RestTemplate restTemplate;
 
-        logger.info("Starting NON-BLOCKING Controller!");
-        Mono<String> orderDrinkResponse = WebClient.create(bartenderApiGraphQlUrl.toString())
-                .post()
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(BodyInserters.fromValue(topologyQuery))
-                .accept(MediaType.APPLICATION_JSON)
-                .retrieve()
-                .bodyToMono(String.class);
+    @GetMapping("/browse/api/restaurants.json")
+    public String browseRestaurants() {
+        String getRestaurantsQuery = asString(resourceLoader.getResource("classpath:/graphql/browse/getRestaurants.graphql"));
 
-        orderDrinkResponse.subscribe(tweet -> logger.info(orderDrinkResponse.toString()));
-        logger.info("Exiting NON-BLOCKING Controller!");
-        return orderDrinkResponse;
+        GraphQLQuery graphQlQuery = new GraphQLQuery();
+        graphQlQuery.setQuery(getRestaurantsQuery);
 
+        GraphQLQueryResponse response = restTemplate.postForObject(bartenderApiGraphQlUrl.toString(), graphQlQuery, GraphQLQueryResponse.class);
+        logger.info("graphQl response: {}", response);
+        String restaurants = response.getData().get("getRestaurants").toString();
+        logger.info("restaurants: {}", restaurants);
+        return restaurants;
+    }
+
+    public String asString(Resource resource) {
+        try (Reader reader = new InputStreamReader(resource.getInputStream(), UTF_8)) {
+            return FileCopyUtils.copyToString(reader);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
     }
 }
